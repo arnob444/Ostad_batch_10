871b75ffd9129f3db64a024a5b8cd61750eb36db
arnob444 <shacha15-5277@diu.edu.bd>
2025-05-16 16:16:36 +0600

    submit

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -8,11 +8,7 @@ class HomeScreen extends StatelessWidget {
   final List<Flag> flags = [
     Flag(title: "Bangladesh", subtitle: "See More", flagAsset: "asset/bd.png"),
     Flag(title: "France", subtitle: "See More", flagAsset: "asset/france1.png"),
-    Flag(
-      title: "Germany",
-      subtitle: "See More",
-      flagAsset: "asset/germany.png",
-    ),
+    Flag(title: "Germany", subtitle: "See More", flagAsset: "asset/germany.png",),
     Flag(title: "Australia", subtitle: "See More", flagAsset: "asset/aus.png"),
     Flag(title: "Spain", subtitle: "See More", flagAsset: "asset/spain.png"),
     Flag(title: "USA", subtitle: "See More", flagAsset: "asset/usa.png"),
